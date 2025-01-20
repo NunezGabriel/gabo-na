b495d1717a9bc470c9e003a83bc2622166c580b5
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2025-01-20 11:15:09 -0500

    chore: services info + data file

---
+++ src/scripts/data.js
@@ -0,0 +1,14 @@
+const services = [
+  {
+    id: 1,
+    title: "",
+    description: "Educación formal, cursos, talleres y charlas.",
+    icon: "fas fa-graduation-cap",
+  },
+  {
+    id: 2,
+    title: "Tecnologías",
+    description: "Tecnologías, herramientas y metodologías.",
+    icon: "fas fa-code",
+  },
+];
